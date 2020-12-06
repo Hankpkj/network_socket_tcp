b73c5695c04b453bb3f66b93ab4e8ea513c8e1f7
hankpkj <hankpkj@gmail.com>
2020-12-06 17:57:17 +0900

    first commit

---
+++ client-side.py
@@ -0,0 +1,170 @@
+from tkinter import Tk, Frame, Scrollbar, Label, END, Entry, Text, VERTICAL, Button
+import socket
+import threading
+from tkinter import messagebox
+
+
+class ClientSide:
+    client_socket = None
+    last_received_message = None
+
+    def __init__(self, master):
+        self.root = master
+        self.chat_transcript_area = None
+        self.name_space = None
+        self.ip_space = None
+        self.port_space = None
+        self.enter_text_widget = None
+        self.join_button = None
+        self.initialize_socket()
+        self.initialize_gui()
+        self.listen_for_incoming_messages_in_a_thread()
+
+    def initialize_socket(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        remote_ip = '127.0.0.1'
+        remote_port = 9999
+        self.client_socket.connect((remote_ip, remote_port))
+
+    def initialize_gui(self):
+        self.root.title("Socket Chat")
+        self.root.resizable(0, 0)
+        self.display_chat_box()
+        self.display_ip_section()
+        self.display_port_section()
+        self.display_name_section()
+        self.display_chat_entry_box()
+
+    def listen_for_incoming_messages_in_a_thread(self):
+        thread = threading.Thread(target=self.receive_message_from_server, args=(self.client_socket,))
+        thread.start()
+
+    def receive_message_from_server(self, so):
+        while True:
+            buffer = so.recv(1024)
+            if not buffer:
+                break
+            message = buffer.decode('utf-8')
+
+            if "joined" in message:
+                user = message.split(":")[1]
+                message = user + " has joined"
+                self.chat_transcript_area.insert('end', message + '\n')
+                self.chat_transcript_area.yview(END)
+            else:
+                self.chat_transcript_area.insert('end', message + '\n')
+                self.chat_transcript_area.yview(END)
+
+        so.close()
+
+    def display_name_section(self):
+        frame = Frame()
+        Label(frame, text='Enter your name:', font=("Helvetica", 16)).pack(side='left', padx=10)
+        self.name_space = Entry(frame, width=50, borderwidth=2)
+        self.name_space.pack(side='left', anchor='e')
+        self.join_button = Button(frame, text="Join", width=10, command=self.usr_connect).pack(side='left')
+        frame.pack(side='top', anchor='nw')
+
+    def display_ip_section(self):
+        frame = Frame()
+        Label(frame, text='Enter ip:', font=("Helvetica", 16)).pack(side='left', padx=10)
+        self.ip_space = Entry(frame, width=50, borderwidth=2)
+        self.ip_space.pack(side='left', anchor='e')
+        self.join_button = Button(frame, text="fix", width=10, command=self.block_ip).pack(side='left')
+        frame.pack(side='top', anchor='nw')
+
+    def display_port_section(self):
+        frame = Frame()
+        Label(frame, text='Enter port', font=("Helvetica", 16)).pack(side='left', padx=10)
+        self.port_space = Entry(frame, width=50, borderwidth=2)
+        self.port_space.pack(side='left', anchor='e')
+        self.join_button = Button(frame, text="fix", width=10, command=self.block_port).pack(side='left')
+        frame.pack(side='top', anchor='nw')
+
+    def display_chat_box(self):
+        frame = Frame()
+        Label(frame, text='Chat Box:', font=("Serif", 12)).pack(side='top', anchor='w')
+        self.chat_transcript_area = Text(frame, width=60, height=10, font=("Serif", 12))
+        scrollbar = Scrollbar(frame, command=self.chat_transcript_area.yview, orient=VERTICAL)
+        self.chat_transcript_area.config(yscrollcommand=scrollbar.set)
+        self.chat_transcript_area.bind('<KeyPress>', lambda e: 'break')
+        self.chat_transcript_area.pack(side='left', padx=10)
+        scrollbar.pack(side='right', fill='y')
+        frame.pack(side='top')
+
+    def display_chat_entry_box(self):
+        frame = Frame()
+        Label(frame, text='Enter message:', font=("Serif", 12)).pack(side='top', anchor='w')
+        self.enter_text_widget = Text(frame, width=60, height=3, font=("Serif", 12))
+        self.enter_text_widget.pack(side='left', pady=15)
+        self.enter_text_widget.bind('<Return>', self.on_enter_key_pressed)
+        frame.pack(side='top')
+
+    def block_ip(self):
+        if len(self.ip_space.get()) == 0:
+            messagebox.showerror(
+                "Enter ip address dd", self.ip_space.get()
+            )
+            return
+        elif self.ip_space.get() != '127.0.0.1':
+            messagebox.showerror(
+                "Enter ip address", self.ip_space.get()
+            )
+            return
+        self.ip_space.config(state='disabled')
+
+    def block_port(self):
+        if len(self.port_space.get()) == 0:
+            messagebox.showerror(
+                "Enter port number", "eg. 9999"
+            )
+            return
+        elif self.port_space.get() != '9999':
+            messagebox.showerror(
+                "Enter port number", "eg. 9999"
+            )
+            return
+        self.port_space.config(state='disabled')
+
+    def usr_connect(self):
+        if len(self.name_space.get()) == 0:
+            messagebox.showerror(
+                "Enter your name", "Enter your name to send a message")
+            return
+
+        self.name_space.config(state='disabled')
+        self.client_socket.send(("joined:" + self.name_space.get()).encode('utf-8'))
+
+    def on_enter_key_pressed(self, event):
+        if len(self.name_space.get()) == 0:
+            messagebox.showerror(
+                "Enter your name", "Enter your name to send a message")
+            return
+        self.send_chat()
+        self.clear_text()
+
+    def clear_text(self):
+        self.enter_text_widget.delete(1.0, 'end')
+
+    def send_chat(self):
+        senders_name = self.name_space.get().strip() + ": "
+        data = self.enter_text_widget.get(1.0, 'end').strip()
+        message = (senders_name + data).encode('utf-8')
+        self.chat_transcript_area.insert('end', message.decode('utf-8') + '\n')
+        self.chat_transcript_area.yview(END)
+        self.client_socket.send(message)
+        self.enter_text_widget.delete(1.0, 'end')
+        return 'break'
+
+    def on_close_window(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.root.destroy()
+            self.client_socket.close()
+            exit(0)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    gui = ClientSide(root)
+    root.protocol("WM_DELETE_WINDOW", gui.on_close_window)
+    root.mainloop()
---
+++ server-side.py
@@ -0,0 +1,79 @@
+import socket
+import threading
+
+
+class ChatServer:
+    clients_list = []
+    last_received_message = ""
+    # for re-use user
+    message_list = []
+    str_client_list = []
+    un_expected_disconnection = []
+
+    def __init__(self):
+        # init socket
+        self.server_socket = None
+        # start listening
+        self.create_listening_server()
+
+    def create_listening_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_ip = '127.0.0.1'
+        # TODO: apply port with input value
+        server_port = 9999
+        # this will allow you to immediately restart a TCP server
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # this makes the server listen to requests coming from other computers on the network
+        self.server_socket.bind((server_ip, server_port))
+
+        print('Start Server !')
+        print('listening connection ...')
+
+        self.server_socket.listen(5)
+        self.receive_messages_in_a_new_thread()
+
+    def receive_messages(self, so):
+        while True:
+            incoming_buffer = so.recv(1024)
+
+            if not incoming_buffer:
+                break
+        self.message_list.append(incoming_buffer.decode('utf-8'))
+        self.broadcast_to_all_clients(so)  # send to all clients
+
+    def broadcast_to_all_clients(self, senders_socket):
+        for client in self.clients_list:
+            sock, (ip, port) = client
+            if sock is not senders_socket:
+                sock.sendall(self.message_list[-1].encode('utf-8'))
+
+            # self.un_expected_disconnection.append(ip + ':' + str(port))
+            # if ip + ':' + str(port) in self.str_client_list:
+            #     self.str_client_list.remove(ip + ':' + str(port))
+
+    def receive_messages_in_a_new_thread(self):
+        while True:
+            # accept
+            client = so, (ip, port) = self.server_socket.accept()
+            print(ip, port)
+
+            # if un_expected_disconnection user connect send list of msg
+            if ip + ':' + str(port) in self.un_expected_disconnection:
+                for msg in self.message_list:
+                    so.sendall(msg.encode('utf-9'))
+                self.un_expected_disconnection.remove(ip + ':' + str(port))
+
+            self.clients_list.append(client)
+            self.str_client_list.append(ip + ':' + str(port))
+
+            t = threading.Thread(target=self.receive_messages, args=(so,))
+            t.start()
+
+            self.un_expected_disconnection.append(ip + ':' + str(port))
+            if ip + ':' + str(port) in self.str_client_list:
+                self.str_client_list.remove(ip + ':' + str(port))
+
+
+
+if __name__ == "__main__":
+    ChatServer()
